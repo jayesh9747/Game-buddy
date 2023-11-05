712527f27b0ce3871adddead9214d969361150bb
Jayesh Savaliya <jayeshsavaliya009@gmail.com>
2023-11-05 11:01:59 +0530

    chakra UI

--- Game-review-client/src/main.tsx
+++ Game-review-client/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const colors = {
+  brand: {
+    900: "#378087",
+    800: "#70B1BA",
+    700: "#BADFE7",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
